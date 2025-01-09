b988d1d61093cf681d21f5c993e81a99a73427b5
Rebecca Peckman <rebeccapeckman@gmail.com>
2025-01-09 16:30:38 -0500

    fixed cors issue

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,12 @@ import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    }
+});
 
 app.get("/", (req, res) => {
     res.send('Hello World');
@@ -16,8 +21,10 @@ server.listen(3000, () => {
 
 io.on("connection", (socket) => {
     console.log("a user connected");
+
+    socket.on("disconnect", () => {
+        console.log("a user disconnected");
+    });
 });
 
-io.on("disconnect", (socket) => {
-    console.log("a user disconnected");
-});
+
