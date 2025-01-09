a666c519ce94f77880adad188a3209fa4c7c7d2a
Rebecca Peckman <rebeccapeckman@gmail.com>
2025-01-09 16:31:59 -0500

    cleanup

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,13 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from "socket.io";
 
-const app = express();
-const server = createServer(app);
+const server = createServer(express());
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:5173",
         methods: ["GET", "POST"],
     }
 });
-
-app.get("/", (req, res) => {
-    res.send('Hello World');
-})
 
 server.listen(3000, () => {
     console.log("server running on port 3000");
